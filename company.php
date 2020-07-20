389129c5fa4d40640644648ec1ab0141f83e2a3a
Wess58 <wesleymbate@gmail.com>
2020-07-20 17:03:06 +0300

    minor tweak

--- company.php
+++ company.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/styles.css">
   <link rel="stylesheet" href="css/style3.css">
-  <link rel="stylesheet" href="css/rellax.css">
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
 
