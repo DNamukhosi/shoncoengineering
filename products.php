75a2c7f424634e8eb9fd57102b2d1ad7a3119f54
Wess58 <wesleymbate@gmail.com>
2020-07-29 16:30:29 +0300

    hot fix

--- products.php
+++ products.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+.php<!DOCTYPE html>
 <html lang="en" dir="ltr">
 
 <head>
@@ -64,23 +64,23 @@
               </ul> -->
             <!-- </li> -->
             <li>
-              <a href="index">Home</a>
+              <a href="index.php">Home</a>
             </li>
 
             <li>
-              <a href="company">Our Company</a>
+              <a href="company.php">Our Company</a>
             </li>
 
             <li>
-              <a href="services">Services</a>
+              <a href="services.php">Services</a>
             </li>
 
             <li>
-              <a href="products">Products</a>
+              <a href="products.php">Products</a>
             </li>
 
             <li>
-              <a href="contact">Contact Us</a>
+              <a href="contact.php">Contact Us</a>
             </li>
           </ul>
         </div>
@@ -117,7 +117,7 @@
   <!-- MAIN NAVBAR -->
 
   <nav class="navbar mainNav fixed-top">
-    <a class="navbar-brand" href="index">
+    <a class="navbar-brand" href="index.php">
       <img src="images/logo.png" alt="logo" id="logo">
     </a>
 
